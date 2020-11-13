288a5b072a06fa5605888dc3598e8a446e92dc22
CarolinaCapilla <carolina.capilla@gmail.com>
2020-11-13 19:21:41 +0000

    added condition to check for ingredients array length and display message to the user according to that length

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients) // we convert our Object into Array to be able to call map on it
+  let transformedIngredients = Object.keys(props.ingredients) // we convert our Object into Array to be able to call map on it
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []); // arr previous value el current value we flatten our previous array to be able to check the length and display JSX depending on that length
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>You can start building your burger!</p>;
+  }
   return (
     <div className={classes["Burger"]}>
       <BurgerIngredient type='bread-top'></BurgerIngredient>
